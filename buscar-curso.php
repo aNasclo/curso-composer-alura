2a053e8bf828fb1f6f43ee5a3f4890d5585d782b
aNasclo <angelojrmelo@gmail.com>
2022-06-21 13:51:00 -0300

    teste

---
+++ buscar-curso.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+// Teste2::teste();
+// exit();
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Alura\BuscadorDeCursos\Buscador;
+
+$client = new Client(['verify' => false, 'base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo exibeMensagem($curso);
+}
---
+++ teste.php
@@ -0,0 +1,17 @@
+<?php
+
+class Teste
+{
+    public static function teste()
+    {
+        echo 'teste';
+    }
+}
+
+class Teste2
+{
+    public static function teste()
+    {
+        echo"teste 2";
+    }
+}
